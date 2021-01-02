fae9dd1c4657395f8035ca74232812ecfeaa9be2
Marc-Antoine Loignon <developer@lognoz.org>
2021-01-02 03:18:40 +0000

    LaTex: Add pip and python version detection statement

--- latex/architect.el
+++ latex/architect.el
@@ -1,14 +1,33 @@
 ;; architect.el --- Architect LaTex Template -*- lexical-binding: t; -*-
 
-(architect-default-directory "~/document/latex/")
+(defun require-program (program)
+  "Check for system PROGRAM and printing error if not found."
+  (unless (executable-find program)
+    (user-error "Required program \"%s\" not found in your path" program)))
 
-(architect-variable
-  :variable "__python_version__"
-  :value "3.7")
+(defvar architect-latex-pip-version
+  (progn
+    (require-program "pip")
+    (string-trim
+      (replace-regexp-in-string ")" ""
+        (shell-command-to-string "pip -V | grep -oE '[^ ]+$'"))))
+  "The current pip version.")
+
+(defvar architect-latex-python-version
+  (let ((python (concat "python" architect-latex-pip-version)))
+    (require-program python)
+    architect-latex-pip-version)
+  "The current python version.")
+
+(architect-default-directory "~/document/latex/")
 
 (architect-variable
   :variable "__date__"
   :value (format-time-string "%B %Y"))
+
+(architect-variable
+  :variable "__python_version__"
+  :value architect-latex-python-version)
 
 (architect-variable
   :variable "__filename__"
